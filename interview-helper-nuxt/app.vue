37872ae88045a5f7a2e561beb5588066123034c1
Ah-ram <arongj6317@gmail.com>
2025-01-08 16:06:05 +0900

    refactor: [정아람] NavigationMenuBar 컴포넌트 분리 [IHF-8]

--- interview-helper-nuxt/app.vue
+++ interview-helper-nuxt/app.vue
@@ -1,30 +1,7 @@
 <template>
     <v-app>
+        <NavigationMenuBar />
         <v-main>
-            <v-app-bar class="navigationbar" :class="{ 'scrolled': isScrolled }" :height="isScrolled ? 50 : 70" color="#1a1a1a" elevation="0">
-                <v-spacer>
-                    <v-btn class="btn-text">
-                        <span>INTERVIEW-HELPER</span>
-                    </v-btn>
-                </v-spacer>
-
-                <v-spacer align="right">
-                    <v-btn class="btn-text" @click="goToInterview" alt="INTERVIEW">
-                        <span>INTERVIEW</span>
-                    </v-btn>
-                    <v-btn class="btn-text" @click="goToBoardList" alt="BOARD">
-                        <span>BOARD</span>
-                    </v-btn>
-
-                    <v-btn class="btn-text" v-if="isAuthenticated == false" @click="goToGoogleLogin" alt="GoogleLogin">
-                        <span>LOGIN</span>
-                    </v-btn>
-
-                    <v-btn class="btn-text" v-if="isAuthenticated == true" @click="goToGoogleLogout" alt="GoogleLogout">
-                        <span>LOGOUT</span>
-                    </v-btn>
-                </v-spacer>
-            </v-app-bar>
             <NuxtPage />
             <!--            <NuxtLink to="/">Home</NuxtLink>-->
         </v-main>
@@ -33,81 +10,13 @@
 
 <script>
 // import router from '~/router/index.ts'
-import { defineComponent, onBeforeUnmount } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { useAuthenticationStore } from './authentication/stores/authenticationStore';
+import NavigationMenuBar from './navigation_bar/NavigationMenuBar.vue';
 
-export default defineComponent({
-    setup() {
-        const route = useRoute()
-        const router = useRouter()
-        const authenticationStore = useAuthenticationStore()
-
-        const isScrolled = computed(() => {
-            if (path.value !== '/') {
-                return false;
-            } else {
-                return Yscrolled.value > 50;
-            }
-        })
-        const isAuthenticated = computed(() => authenticationStore.isAuthenticated)
-        const path = computed(() => route.path)
-        const Yscrolled = ref(0)
-
-        function goToBoardList() {
-            router.push("/board/list")
-        }
-
-        function goToInterview() {
-            router.push("/interview/category")
-        }
-
-        async function goToGoogleLogin() {
-            await authenticationStore.requestGoogleOauthRedirectionToSpring();
-        }
-
-        async function goToGoogleLogout() {
-            await authenticationStore.requestGoogleLogoutToSpring();
-            router.push("/")
-        }
-
-        function handleScroll() {
-            Yscrolled.value = window.scrollY
-        }
-
-        onMounted(async () => {
-            if (authenticationStore.isAuthenticated != undefined) {
-
-                if (!authenticationStore.isAuthenticated) {
-                    localStorage.removeItem('userToken')
-                }
-            }
-
-            window.addEventListener('scroll', handleScroll)
-        })
-
-        onBeforeUnmount(() => {
-            window.removeEventListener('scroll', handleScroll)
-        })
-
-        return {
-            isScrolled,
-            isAuthenticated,
-
-            goToBoardList,
-            goToInterview,
-            goToGoogleLogin,
-            goToGoogleLogout,
-            handleScroll,
-        }
-    }
-})
+export default {
+    components: {
+        NavigationMenuBar
+    },
+}
 </script>
 
-<style scoped>
-.btn-text {
-    color: white;
-    font-size: 0.75rem;
-}
 
-</style>
---
+++ interview-helper-nuxt/navigation_bar/NavigationMenuBar.vue
@@ -0,0 +1,136 @@
+<template>
+    <v-app-bar class="navigationbar" :class="{ 'scrolled': isScrolled }" :height="isScrolled ? 50 : 70" color="#1a1a1a" elevation="0">
+        <v-spacer>
+            <v-btn class="btn-text">
+                <span>INTERVIEW-HELPER</span>
+            </v-btn>
+        </v-spacer>
+
+        <v-spacer align="right">
+            <v-btn class="btn-text" @click="goToInterview" alt="INTERVIEW">
+                <span>INTERVIEW</span>
+            </v-btn>
+            <v-btn class="btn-text" @click="goToBoardList" alt="BOARD">
+                <span>BOARD</span>
+            </v-btn>
+
+            <v-btn class="btn-text" v-if="isAuthenticated == false" @click="goToGoogleLogin" alt="GoogleLogin">
+                <span>LOGIN</span>
+            </v-btn>
+
+            <v-btn class="btn-profile" v-if="isAuthenticated == true" @click="goToMyPage" alt="MyPage">
+                <img :src="userPicture" alt="userPicture" class="profile-picture"/>
+            </v-btn>
+        </v-spacer>
+     </v-app-bar>
+</template>
+
+<script>
+import { useRoute, useRouter } from 'vue-router';
+import { useAuthenticationStore } from '../authentication/stores/authenticationStore';
+import { useUserProfileStore } from '../user_profile/stores/UserProfileStore';
+import { defineComponent, onBeforeUnmount, ref, computed, watch, onMounted } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const route = useRoute()
+        const router = useRouter()
+        const authenticationStore = useAuthenticationStore()
+        const userProfileStore = useUserProfileStore()
+
+        const isScrolled = computed(() => {
+            if (path.value !== '/') {
+                return false;
+            } else {
+                return Yscrolled.value > 50;
+            }
+        })
+        const isAuthenticated = computed(() => authenticationStore.isAuthenticated)
+        const path = computed(() => route.path)
+        const Yscrolled = ref(0)
+        const userPicture = ref(null)
+
+        function goToBoardList() {
+            router.push("/board/list")
+        }
+
+        function goToInterview() {
+            router.push("/interview")
+        }
+
+        async function goToGoogleLogin() {
+            await authenticationStore.requestGoogleOauthRedirectionToSpring();
+        }
+
+        async function goToMyPage() {
+            await router.push("/user-profile")
+        }
+
+        function handleScroll() {
+            Yscrolled.value = window.scrollY
+        }
+
+
+        onMounted(async () => {
+            if (authenticationStore.isAuthenticated != undefined) {
+
+                if (!authenticationStore.isAuthenticated) {
+                    localStorage.removeItem('userToken')
+                }
+            }
+
+            window.addEventListener('scroll', handleScroll)
+        })
+
+        onBeforeUnmount(() => {
+            window.removeEventListener('scroll', handleScroll)
+        })
+
+        watch(() => authenticationStore.isAuthenticated,
+            async (newVal) => {
+                if (newVal === true) {
+                    try {
+                        const response = await userProfileStore.requestUserInfoToSpring()
+                        console.log("picture: ", response.picture)
+                        userPicture.value = response.picture
+                        console.log("userPicture.value: ", userPicture.value)
+                    } catch (error) {
+                        console.error("UserPicture 가져오는 중 에러 발생: ", error)
+                    }
+                }
+            }
+        )
+
+        return {
+            isScrolled,
+            isAuthenticated,
+            userPicture,
+
+            goToBoardList,
+            goToInterview,
+            goToGoogleLogin,
+            goToMyPage,
+            handleScroll,
+        }
+    }
+})
+</script>
+
+<style scoped>
+.btn-text {
+    color: white;
+    font-size: 0.75rem;
+}
+
+.btn-profile {
+    padding: 0;
+}
+
+.profile-picture {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
+</style>

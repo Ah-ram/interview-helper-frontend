1a5eaa2344f6ddc741b2f5601e1415d4feb23089
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2025-01-13 12:26:17 +0900

    fix: [한재혁] 생성된 질문을 제대로 받아올 수 있도록 수정 [IHF-13]

--- interview-helper-nuxt/interview/stores/interviewStore.ts
+++ interview-helper-nuxt/interview/stores/interviewStore.ts
@@ -19,11 +19,23 @@ export const useInterviewStore = defineStore('interviewStore', {
         async requestGeneratedQuestionsResultToFastAPI() {
             const { fastapiAxiosInst } = createAxiosInstances()
 
+            const maxAttempts = 10
+            const delay = 1000
+
             try {
                 const userToken = localStorage.getItem("userToken")
-                const response = await fastapiAxiosInst.post("/generate-questions-result", { userToken })
-                
-                return response.data.message;
+                for (let attempt = 0; attempt < maxAttempts; attempt++) {
+                    const response = await fastapiAxiosInst.post("/generate-questions-result", { userToken })
+                    console.log("generatedQuestions: ", response.data)
+
+                    if (response.data) {
+                        return response.data;
+                    }
+                    
+                    console.log(`${attempt + 1}번째 요청 실패!`)
+                    await new Promise((resolve) => setTimeout(resolve, delay))
+                }
+                throw new Error("면접 질문을 가져오는데 실패했습니다.")
             } catch (error) {
                 console.error("requestGeneratedQuestionsResultToFastAPI() 중 에러 발생: ", error)
             }

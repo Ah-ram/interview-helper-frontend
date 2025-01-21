828d83fdb553614729e6e24a7cee750a5c56bff3
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2025-01-21 15:41:35 +0900

    feat: [한재혁] 서비스 홈페이지를 구성합니다. [IHF-17]

--- interview-helper-nuxt/pages/index.vue
+++ interview-helper-nuxt/pages/index.vue
@@ -3,15 +3,33 @@
     <v-app>
         <v-main>
             <nav>
-                <nuxt-link to="/board/list">게시글 목록</nuxt-link>
-            </nav>
-<!--            <NuxtPage />-->
+                <div class="homepage">
+                    <h2 class="main-text">서비스 준비중입니다!</h2>
+                    <home />
+                </div>  
+            </nav>            
         </v-main>
     </v-app>
 </template>
 
 <script setup>
+import home from './home.vue';
 </script>
 
 <style scoped>
+.main-text {
+    position: absolute;
+    font-size: 1rem;
+    color: #fff;
+    text-align: center;
+}
+.homepage {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    scale: 3
+}
 </style>
